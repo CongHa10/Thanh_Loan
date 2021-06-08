72992e54abafdb745d80d2a7f1993a4e302d85e3
CongHa10 <52393812+CongHa10@users.noreply.github.com>
2021-06-08 10:47:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '<3 ( ấn vào đây đê )',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên @@@',
-    desc: 'Anh cũng chẳng biết nữa, nhưng mà lỡ rồi thì phải nói thôi. Anh không biết diễn tả như nào nữa, nhưng mà anh chỉ biết rằng chỉ cần được cạnh em là anh quên hết mọi thứ chỉ còn có mỗi em, anh lỡ thích em mất rồi. Covid thì anh không dính nhưng mà tương tư về em thì anh dương tính mất rồi. Mình có thể tiến xa hơn mối quan hệ bạn bè được không ??? ',
+    desc: 'Anh cũng chẳng biết nữa, nhưng mà lỡ rồi thì phải nói thôi. Anh không biết diễn tả như nào nữa, nhưng mà anh chỉ biết rằng chỉ cần được cạnh em là anh quên hết mọi thứ chỉ còn có mỗi em, anh lỡ thích em mất rồi. Mình có thể tiến xa hơn mối quan hệ bạn bè được không ??? ',
     btnYes: 'Okei "chú" ',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người nhưng mà anh lỡ yêu em mất rồi <3',
@@ -14,6 +14,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu ❤️❤️❤️',
     mess: 'Anh chuẩn bị để về đúng ngày sinh nhật em nhưng mà dịch lại không về được. Xin lỗi em !',
     messDesc: 'Để qua đợt này rồi mình sẽ đi những nơi mà lúc trước mình đã dự định nha nhaaaa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiie lun <3',
     messLink: 'https://www.facebook.com/cong.ha.1010/' 
 }
